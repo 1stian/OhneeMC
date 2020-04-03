4e4bdbb95a0d3f7ea36c7b864ddc941e16bf4c28
Stian Tofte <stian@realnaits.com>
2020-04-04 00:52:51 +0200

    Added documentation.

--- src/main/java/com/ohneemc/ohneemc/helpers/MessageHelper.java
+++ src/main/java/com/ohneemc/ohneemc/helpers/MessageHelper.java
@@ -5,6 +5,11 @@ import org.bukkit.entity.Player;
 
 public class MessageHelper {
 
+    /**
+     *
+     * @param player Player to send message to.
+     * @param message The message.
+     */
     public static void sendMessage(Player player, String message) {
         if (player != null && message != null){
             player.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "Ohnee" + ChatColor.GOLD + "] " + ChatColor.WHITE + message);
