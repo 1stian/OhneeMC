a3cf127f472651d9ecda37308bbc338b1b080ac9
Stian Tofte <stian@realnaits.com>
2020-04-04 00:52:22 +0200

    Added documentation.

--- src/main/java/com/ohneemc/ohneemc/helpers/Teleport.java
+++ src/main/java/com/ohneemc/ohneemc/helpers/Teleport.java
@@ -5,6 +5,12 @@ import org.bukkit.entity.Player;
 
 public class Teleport {
 
+    /**
+     *
+     * @param player The player you want to teleport to target.
+     * @param target The target for player.
+     * @return Tru if successful. Otherwise false.
+     */
     public static boolean tpPlayerToPlayer(Player player, Player target){
         if (player != null && target != null){
             player.teleport(target);
@@ -13,6 +19,12 @@ public class Teleport {
         return false;
     }
 
+    /**
+     *
+     * @param player The player to teleport.
+     * @param location Destination of teleport.
+     * @return Tru if successful. Otherwise false.
+     */
     public static boolean tpPlayerToLocation(Player player, Location location){
         return player.teleport(location);
     }
