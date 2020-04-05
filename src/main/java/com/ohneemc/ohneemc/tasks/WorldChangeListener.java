d7fe402cba5acd3b0f5f5aa7676f611b520a847c
Stian Tofte <stian@realnaits.com>
2020-04-05 16:49:08 +0200

    Added @EventHandler.

--- src/main/java/com/ohneemc/ohneemc/tasks/WorldChangeListener.java
+++ src/main/java/com/ohneemc/ohneemc/tasks/WorldChangeListener.java
@@ -3,11 +3,13 @@ package com.ohneemc.ohneemc.tasks;
 import com.ohneemc.ohneemc.util.UserData;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 public class WorldChangeListener implements Listener {
 
+    @EventHandler
     public void worldChange(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
 
